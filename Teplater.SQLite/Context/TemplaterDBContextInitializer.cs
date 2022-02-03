7cb1ddd3b9730301309d2630142fb5a8d44d92ed
гера шлычков <germean322@gmail.com>
2022-02-03 17:41:11 +1000

    Фикс дб

--- Teplater.SQLite/Context/TemplaterDBContextInitializer.cs
+++ Teplater.SQLite/Context/TemplaterDBContextInitializer.cs
@@ -3,18 +3,18 @@ using Microsoft.EntityFrameworkCore.Design;
 
 namespace Teplater.SQLite.Context
 {
-    class TemplaterDBContextInitializer : IDesignTimeDbContextFactory<TeplaterSQLDB>
-    {
-        public TeplaterSQLDB CreateDbContext(string[] args)
-        {
-            var optionBuilder = new DbContextOptionsBuilder<TeplaterSQLDB>();
+    //class TemplaterDBContextInitializer : IDesignTimeDbContextFactory<TeplaterSQLDB>
+    //{
+    //    public TeplaterSQLDB CreateDbContext(string[] args)
+    //    {
+    //        var optionBuilder = new DbContextOptionsBuilder<TeplaterSQLDB>();
 
-            optionBuilder
-                .UseLazyLoadingProxies()
-                .UseSqlite("Data Source=TemplaterDB.db;Cache=Shared")
-                ;
+    //        optionBuilder
+    //            .UseLazyLoadingProxies()
+    //            .UseSqlite("Data Source=TemplaterDB.db;Cache=Shared")
+    //            ;
 
-            return new TeplaterSQLDB(optionBuilder.Options);
-        }
-    }
+    //        return new TeplaterSQLDB(optionBuilder.Options);
+    //    }
+    //}
 }
