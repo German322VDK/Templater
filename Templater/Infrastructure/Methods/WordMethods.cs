352f741c85d3c402fc98f1b40c03479474d7ca81
гера шлычков <germean322@gmail.com>
2022-02-04 18:21:34 +1000

    метод для поиска всех меток в шаблоне

---
+++ Templater/Infrastructure/Methods/WordMethods.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Templater.Infrastructure.Methods
+{
+    public static class WordMethods
+    {
+        //ищет в тексте все подстроки типа "from...to"
+        public static ICollection<string> SearchFromTo(string s, char from, char to)
+        {
+            var strs = new List<string>();
+
+            List<char> str = new List<char>();
+
+            bool flag = false;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == from)
+                {
+                    str = new List<char>();
+
+                    flag = true;
+                }
+                if (flag)
+                {
+                    str.Add(s[i]);
+                }
+
+                if (s[i] == to)
+                {
+                    flag = false;
+
+                    strs.Add(new string(str.ToArray()));
+                }
+            }
+
+            return strs;
+        }
+    }
+}
