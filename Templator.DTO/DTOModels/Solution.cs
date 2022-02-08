7a8b3d75011dbf93c4a3bc2ecabdfe014bda6102
Илья Тимофеев <Mcdos14@ya.ru>
2022-02-08 21:50:39 +0500

    Добавлен класс решения(получаемого от робота процессора)

---
+++ Templator.DTO/DTOModels/Solution.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Templator.DTO.DTOModels.Base;
+
+namespace Templator.DTO.DTOModels
+{
+   public class Solution : Entity    //класс решения от робота
+    {
+        public virtual Template Template { get; set; } //нужный нам шаблон
+
+        public Dictionary<string, string> Data { get; set; } //данные (метка-значение)
+    }
+}
