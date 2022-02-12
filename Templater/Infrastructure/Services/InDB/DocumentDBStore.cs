8d97e592eb3a5037d1631434ae6ce67865b1b434
Илья Тимофеев <Mcdos14@ya.ru>
2022-02-12 22:06:35 +0500

    Правка отображения

--- Templater/Infrastructure/Services/InDB/DocumentDBStore.cs
+++ Templater/Infrastructure/Services/InDB/DocumentDBStore.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using Templater.Infrastructure.Interfaces;
 using Templator.DTO.DTOModels;
