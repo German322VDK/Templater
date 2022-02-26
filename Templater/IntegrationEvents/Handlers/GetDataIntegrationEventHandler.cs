bc939a3d63fd7b34b78ab26a8026d19690b21df7
Илья Тимофеев <Mcdos14@ya.ru>
2022-02-26 17:19:02 +0500

    простейшая(и наверное неправильная ) обработка события

--- Templater/IntegrationEvents/Handlers/GetDataIntegrationEventHandler.cs
+++ Templater/IntegrationEvents/Handlers/GetDataIntegrationEventHandler.cs
@@ -1,5 +1,7 @@
 ﻿using EventBus.Base.Standard;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Templater.IntegrationEvents.Events;
 
@@ -7,9 +9,10 @@ namespace Templater.IntegrationEvents.Handlers
 {
     public class GetDataIntegrationEventHandler : IIntegrationEventHandler<ItemCreatedIntegrationEvent>
     {
+        public ICollection<ItemCreatedIntegrationEvent> Events;
         public Task Handle(ItemCreatedIntegrationEvent @event)
         {
-
+            Events.Add(@event);
             throw new NotImplementedException();
         }
 
